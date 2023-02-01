27490c29d0dd76f277c8fda3ab6d25cb963dc862
threefive4 <120577545+threefive4@users.noreply.github.com>
2023-02-01 11:57:31 +0300

    Add files via upload

---
+++ src/ua/lviv/lgs/AircraftCapabilities.java
@@ -0,0 +1,9 @@
+package ua.lviv.lgs;
+
+public interface AircraftCapabilities {
+    void turboAcceleration();
+
+    void stealthTechnology();
+
+    void nuclearStrike();
+}
---
+++ src/ua/lviv/lgs/Main.java
@@ -0,0 +1,22 @@
+package ua.lviv.lgs;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Su27 su = new Su27(2500, "black");
+        su.startingEngines();
+        su.aircraftTakingOff();
+        su.moveUp();
+        su.moveLeft();
+        su.moveDown();
+        su.moveRight();
+        su.stealthTechnology();
+        su.nuclearStrike();
+        su.turboAcceleration();
+        su.landingThePlane();
+
+
+    }
+}
+
+
